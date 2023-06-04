80236c53ef844c21c161ee5b777d33b78b7a05cc
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2023-06-04 05:14:02 -0500

    Update index.js

--- client/src/components/landingPage/utility_components/index.js
+++ client/src/components/landingPage/utility_components/index.js
@@ -1,5 +1,5 @@
 export { default as Info } from "./Info";
 export { default as ScrollUp } from "./ScrollUp";
-export { default as ScrollIndicator } from "./scrollIndicator";
+//export { default as ScrollIndicator } from "./scrollIndicator";
 export { default as Scrollbar } from "./Scrollbar";
 export { default as Cookies } from "./Cookies";
